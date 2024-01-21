fed4c1e57386dffbf79aba90d1f77c99dbc7ed6b
Seyedmasih Tabaei <seyedmasih.tabaei@stud.hs-coburg.de>
2024-01-21 23:55:08 +0100

    Cleaned up the project and documented the source and header files

--- cobit.c
+++ cobit.c
@@ -1,29 +1,61 @@
 #include "cobit.h"
 
+// Defining the previously declared variable for keeping track of test results generated and initializing with 0
 int test_result_counter = 0;
+// Defining the previously declared array for storing the output messages (as results) of tests
 char test_results[MAX_NUM_STRINGS][MAX_STR_LEN];
 
+/*
+	Defining the previously declared assert function
+	which should test a case (an assertion) and return the result.
+
+	Parameter(s):
+		- char* successed_message (the message that will be shown if the test succeeded)
+		- char* failed_message (the message that will be shown if the test faild)
+		- bool test_to_be_done (the case/assertion to test)
+	Return-Value(s):
+		- Test_Result_Info (the resulf of the test executed)
+*/ 
 Test_Result_Info cobit_assert(char* successed_message, char* failed_message, bool test_to_be_done)
 {
-	
+	// Instantiating the struct to store result of the test
 	Test_Result_Info test_result;
+	// Checking if the assertion is true
 	if(test_to_be_done)
 	{
+		// Setting the status of the result so that indicates that the assertion was true
 		test_result.status = 1;
+		// Assigning the corresponding message
 		test_result.message = successed_message;
 	}
 	else
 	{
+		// Setting the status of the result so that indicates that the assertion was wrong
 		test_result.status = 0;
+		// Assigning the corresponding message
 		test_result.message = failed_message;
 	}
+
+	// Returning the result
 	return test_result;
 }
 
+/*
+	Defining the previously declared assert function
+	which should test a case (an assertion) and return the result.
+
+	Parameter(s):
+		- Test_Function test_functions[] (the array containg all tests [functions representing the tests])
+		- int number_of_test_functions (the size of the array containing all tests)
+	Return-Value(s):
+		- void
+*/ 
 void run_all_cobit_tests(Test_Function test_functions[], int number_of_test_functions)
 {
+	// Iterating over the array of tests
 	for(int i = 0; i < number_of_test_functions; i++)
 	{
+		// Executing/running all the tests and displaying their result
 		sprintf(test_results[test_result_counter++], "%s", test_functions[i]().message);	
 	}
 }
--- cobit.h
+++ cobit.h
@@ -3,24 +3,29 @@
 #include <stdbool.h>
 #include <stdio.h>
 
+// Defining a macro for defining the maximum length of each line of the output message (= number of columns)
 #define MAX_STR_LEN 50
+// Defining a macro for defining the maximum number of lines serving as output messages
 #define MAX_NUM_STRINGS 5
 
+// Declaring a variable for keeping track of test results generated (definition delegated to the source file)
 extern int test_result_counter;
+// Declaring an array for storing the output messages (as results) of tests (definition delegated to the source file)
 extern char test_results[MAX_NUM_STRINGS][MAX_STR_LEN];
 
+// Defining a struct as a type with two members storing message and status of a test
+typedef struct test_result_info
+{
+	char *message;
+	bool status;
+} Test_Result_Info;
 
-
-struct test_result_info {
-		char* message;
-		bool status;
-};
- 
-typedef struct test_result_info Test_Result_Info;
+// Defining a function pointer as a type for test functions which return the test result
 typedef Test_Result_Info (*Test_Function)(void);
 
-void myFunction(int parameter);
+// Declaring a function (prototype) which should execute/run all of the tests available
 void run_all_cobit_tests(Test_Function test_functions[], int number_of_test_functions);
-Test_Result_Info cobit_assert(char* successed_message, char* failed_message, bool test_to_be_done);
+// Declaring a function (prototype) which should test a case (an assertion) and return the result 
+Test_Result_Info cobit_assert(char *successed_message, char *failed_message, bool test_to_be_done);
 
 #endif
